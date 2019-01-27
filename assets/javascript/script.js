edd28ed2fef77156b11f75d185487778ee33319c
deanmixer <deanmixer@users.noreply.github.com>
2019-01-26 21:21:42 -0500

    Homework update

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+let username = "deanmixer";
+let hostname = "yahoo.com";
+let linktext = username + "@" + hostname;
+
+$(".emailTarget").html(" <a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
